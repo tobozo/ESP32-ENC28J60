77aff9a93b31525234a1338643a8523d69aa5350
WebSpiderTeam <webspiderteam@hotmail.com>
2024-06-02 13:16:52 +0300

    Fix for getting esp32 default MAC adress

--- src/ESP32-ENC28J60.h
+++ src/ESP32-ENC28J60.h
@@ -23,56 +23,56 @@
 #include "WiFi.h"
 #include "esp_system.h"
 #include "esp_eth.h"
-
+static uint8_t ENC28J60_Default_Mac[] = { 0xFE, 0xED, 0xDE, 0xAD, 0xBE, 0xEF };
 #if ESP_IDF_VERSION_MAJOR < 4 || ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4,4,0)
-  #error "This version of Arduino is too old"
+#error "This version of Arduino is too old"
 #endif
 
 
 class ENC28J60Class {
-    private:
-        bool initialized;
-        bool staticIP;
+private:
+    bool initialized;
+    bool staticIP;
+    uint8_t mac_eth[6] = { 0xFE, 0xED, 0xDE, 0xAD, 0xBE, 0xEF };
+    esp_eth_handle_t eth_handle;
 
-        esp_eth_handle_t eth_handle;
+protected:
+    bool started;
+    eth_link_t eth_link;
+    static void eth_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 
-    protected:
-        bool started;
-        eth_link_t eth_link;
-        static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+public:
+    ENC28J60Class();
+    ~ENC28J60Class();
 
-    public:
-        ENC28J60Class();
-        ~ENC28J60Class();
+    bool begin(int MISO_GPIO, int MOSI_GPIO, int SCLK_GPIO, int CS_GPIO, int INT_GPIO, int SPI_CLOCK_MHZ, int SPI_HOST, uint8_t* ENC28J60_Mac = ENC28J60_Default_Mac);
 
-        bool begin(int MISO_GPIO, int MOSI_GPIO, int SCLK_GPIO, int CS_GPIO, int INT_GPIO, int SPI_CLOCK_MHZ, int SPI_HOST, bool use_mac_from_efuse=false);
+    bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = (uint32_t)0x00000000, IPAddress dns2 = (uint32_t)0x00000000);
 
-        bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = (uint32_t)0x00000000, IPAddress dns2 = (uint32_t)0x00000000);
+    const char* getHostname();
+    bool setHostname(const char* hostname);
 
-        const char * getHostname();
-        bool setHostname(const char * hostname);
+    bool fullDuplex();
+    bool linkUp();
+    uint8_t linkSpeed();
 
-        bool fullDuplex();
-        bool linkUp();
-        uint8_t linkSpeed();
+    bool enableIpV6();
+    IPv6Address localIPv6();
 
-        bool enableIpV6();
-        IPv6Address localIPv6();
+    IPAddress localIP();
+    IPAddress subnetMask();
+    IPAddress gatewayIP();
+    IPAddress dnsIP(uint8_t dns_no = 0);
 
-        IPAddress localIP();
-        IPAddress subnetMask();
-        IPAddress gatewayIP();
-        IPAddress dnsIP(uint8_t dns_no = 0);
+    IPAddress broadcastIP();
+    IPAddress networkID();
+    uint8_t subnetCIDR();
 
-        IPAddress broadcastIP();
-        IPAddress networkID();
-        uint8_t subnetCIDR();
+    uint8_t* macAddress(uint8_t* mac);
+    String macAddress();
 
-        uint8_t * macAddress(uint8_t* mac);
-        String macAddress();
-
-        friend class WiFiClient;
-        friend class WiFiServer;
+    friend class WiFiClient;
+    friend class WiFiServer;
 };
 
 extern ENC28J60Class ETH;
